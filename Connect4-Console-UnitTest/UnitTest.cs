7030f2bea43ba2a6f3d6a38b77b9bb24ab1afa36
rc <hjc1983@gmail.com>
2017-09-21 08:24:40 +1000

    initial check in - unit test and program

---
+++ Connect4-Console-UnitTest/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Connect4_Console;
+
+namespace Connect4_Console_UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void isEmptyGame()
+        {
+            Program p = new Program();
+            Assert.AreEqual(0, p.countDiskOnBoard());
+        }
+    }
+}
--- Connect4-Console/Program.cs
+++ Connect4-Console/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Connect4_Console
 {
-    class Program
+    public class Program
     {
+        //TODO: no_of_rows / no_of_columns to be variables
+        public static int board_rows = 5; //int.Parse(ConfigurationManager.AppSettings["rows"]);
+        public static int board_columns = 5;
+        public string[,] board = new string[board_rows, board_columns];
+        public static string Empty = "O";
+        public static string Red = "R";
+        public static string Yellow = "Y";
+
+
+        public Program()
+        {
+            // populating empty board       
+            for (int row = 0; row < board_rows; row++)
+            {
+                for (int column = 0; column < board_columns; column++)
+                {
+                    board[row, column] = Empty;
+                }
+            }
+        }
+
+        public int countDiskOnBoard()
+        {
+            int count = 0;
+            for (int row = 0; row < board_rows; row++)
+            {
+                for (int column = 0; column < board_columns; column++)
+                {
+                    if (board[row, column] != Empty) count++;
+                }
+            }
+            return count;
+        }
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            
+
+            Console.Read();
         }
     }
 }
