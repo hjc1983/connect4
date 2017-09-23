8540785072d18164ad5cb4f02f4374244eb310d7
robert.chiu <robert.chiu@TA030456>
2017-09-24 08:52:30 +1000

    InsertDiscInColumn and validation

--- Connect4-Console-UnitTest/UnitTest.cs
+++ Connect4-Console-UnitTest/UnitTest.cs
@@ -8,10 +8,55 @@ namespace Connect4_Console_UnitTest
     public class UnitTest
     {
         [TestMethod]
-        public void isEmptyGame()
+        public void IsANewGame()
         {
             Program p = new Program();
-            Assert.AreEqual(0, p.countDiskOnBoard());
+            Assert.AreEqual(0, p.CountDiscsOnBoard());
+        }
+
+        [TestMethod]
+        public void IsNotANewGame()
+        {
+            Program p = new Program();
+            p.Board[4, 1] = "R"; //manually inserted
+            Assert.AreNotEqual(Program.BoardRows * Program.BoardColumns, p.CountDiscsOnBoard());
+        }
+
+
+        [TestMethod]
+        public void ValidateGameUserinput()
+        {
+            Assert.IsFalse(Program.ValidateGameUserinput("aaa"));
+            Assert.IsFalse(Program.ValidateGameUserinput("1 a"));
+            Assert.IsFalse(Program.ValidateGameUserinput("a 2"));
+            Assert.IsFalse(Program.ValidateGameUserinput("@ 2"));
+            Assert.IsFalse(Program.ValidateGameUserinput("ab  3"));
+            Assert.IsFalse(Program.ValidateGameUserinput("2 3 @"));
+            Assert.IsTrue(Program.ValidateGameUserinput(" 2 3 "));
+            Assert.IsTrue(Program.ValidateGameUserinput("5 5"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException), "No more room in this column.")]
+        public void ValidateInsertDiscInColumn()
+        {
+            Program p = new Program();
+            p.InsertDiscInColumn(1);
+            Assert.AreEqual(1, p.CountDiscsOnBoard());
+
+            p.InsertDiscInColumn(1);
+            Assert.AreEqual(2, p.CountDiscsOnBoard());
+
+            p.InsertDiscInColumn(1);
+            Assert.AreEqual(3, p.CountDiscsOnBoard());
+            p.InsertDiscInColumn(1);
+            Assert.AreEqual(4, p.CountDiscsOnBoard());
+
+            p.InsertDiscInColumn(1);
+            Assert.AreEqual(5, p.CountDiscsOnBoard());
+
+            p.InsertDiscInColumn(1);
+            Assert.Fail("Column is full");
         }
     }
 }
--- Connect4-Console/Program.cs
+++ Connect4-Console/Program.cs
@@ -1,48 +1,118 @@
 ﻿using System;
-using System.Configuration;
 
 namespace Connect4_Console
 {
     public class Program
     {
-        //TODO: no_of_rows / no_of_columns to be variables
-        public static int board_rows = 5; //int.Parse(ConfigurationManager.AppSettings["rows"]);
-        public static int board_columns = 5;
-        public string[,] board = new string[board_rows, board_columns];
+        public static int BoardRows { get; set; }
+        public static int BoardColumns { get; set; }
+        public string[,] Board;
         public static string Empty = "O";
         public static string Red = "R";
         public static string Yellow = "Y";
 
-
-        public Program()
+        public Program(int rows = 5, int cols = 5)
         {
+            Board = new string[BoardRows = rows, BoardColumns = cols];
             // populating empty board       
-            for (int row = 0; row < board_rows; row++)
+            for (int row = 0; row < rows; row++)
             {
-                for (int column = 0; column < board_columns; column++)
+                for (int column = 0; column < cols; column++)
                 {
-                    board[row, column] = Empty;
+                    Board[row, column] = Empty;
                 }
             }
         }
 
-        public int countDiskOnBoard()
+        public int CountDiscsOnBoard()
         {
             int count = 0;
-            for (int row = 0; row < board_rows; row++)
+
+            for (int column = 0; column < BoardColumns; column++)
             {
-                for (int column = 0; column < board_columns; column++)
-                {
-                    if (board[row, column] != Empty) count++;
-                }
+                count += CountDiscsInColumn(column);
             }
+
             return count;
+        }
+
+        private int CountDiscsInColumn(int column)
+        {
+            var count = 0;
+            for (int row = 0; row < BoardRows; row++)
+            {
+                if (Board[row, column] != Empty) count++;
+            }
+
+            return count;
+        }
+
+        public static bool ValidateGameUserinput(string input)
+        {
+            int rows, cols;
+
+            var inputs = input.Trim().Split(' ');
+
+            if (inputs.Length != 2 || !int.TryParse(inputs[0], out rows) || !int.TryParse(inputs[1], out cols))
+            {
+                Console.WriteLine("Not a valid input, please try again.");
+                return false;
+            }
+            BoardRows = rows;
+            BoardColumns = cols;
+            return true;
+        }
+
+        public int InsertDiscInColumn(int column)
+        {
+          
+            int row = CountDiscsInColumn(column);
+
+            if (row == BoardRows)
+                throw new ApplicationException("Column is full");
+
+            Board[row, column] = "A";
+            DisplayBoard(Board);
+            
+            return row;
+        }
+        
+        public bool isFinished()
+        {
+            return CountDiscsOnBoard() == BoardRows * BoardColumns;
+        }
+
+        public void DisplayBoard(string[,] board)
+        {
+            int i, ix;
+
+            for (i = 0; i < BoardRows; i++)
+            {
+                Console.Write("|");
+                for (ix = 0; ix < BoardColumns; ix++)
+                {
+                    Console.Write(board[i, ix]);
+                }
+
+                Console.Write("| \n");
+            }
+            Console.WriteLine("\n");
         }
 
         static void Main(string[] args)
         {
-            Program p = new Program();
-            
+            Console.Write("Enter number of rows/cols: ");
+
+            while (!Program.ValidateGameUserinput(Console.ReadLine())) ;
+
+            Console.WriteLine(BoardRows + " x " + BoardColumns);
+            Program p = new Program(BoardRows, BoardColumns);
+
+            while (!p.isFinished())
+            {
+                Console.Write("Where would you like to place your next disc?\n");
+                p.InsertDiscInColumn(Convert.ToInt32(Console.ReadLine()));
+            }
 
             Console.Read();
         }
